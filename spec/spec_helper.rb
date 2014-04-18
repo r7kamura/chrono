93334fbf793d70e3b5991c4fbfb622ddc5fb7174
Ryo Nakamura <r7kamura@gmail.com>
2014-04-19 05:04:08 +0900

    Integrate simplecov with coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
-require "coveralls"
-Coveralls.wear!
-
 require "simplecov"
+require "coveralls"
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
 SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
