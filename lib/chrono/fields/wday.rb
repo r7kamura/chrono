e69bde00e16005bf8ec960789f4132f564014499
NARUSE, Yui <naruse@airemix.jp>
2015-12-04 12:13:44 +0900

    Support names for month and wday
    * Support names for month
    * Support names for day of week
    * Allow 7 as 0 (Sunday) for day of week
    * note: manpages say "Ranges or lists of names are not allowed." but
      actually they are allowed.
    
    http://linux.die.net/man/5/crontab

--- lib/chrono/fields/wday.rb
+++ lib/chrono/fields/wday.rb
@@ -1,7 +1,30 @@
 module Chrono
   module Fields
     class Wday < Base
+      TABLE = {
+        '7'   => '0',
+        'sun' => '0',
+        'mon' => '1',
+        'tue' => '2',
+        'wed' => '3',
+        'thu' => '4',
+        'fri' => '5',
+        'sat' => '6',
+      }
+      REGEXP = %r<\A(?:(?<step>(?:\*|(?:(?<atom>\d+|sun|mon|tue|wed|thu|fri|sat)(?:-\g<atom>)?))(?:/\d+)?)(?:,\g<step>)*)\z>ix
+
+      def initialize(source)
+        unless REGEXP =~ source
+          raise InvalidField.new('Unparsable field', source)
+        end
+        @source = source
+      end
+
       private
+
+      def interpolated
+        super.downcase.gsub(/7|sun|mon|tue|wed|thu|fri|sat/){TABLE[$&]}
+      end
 
       def range
         0..6
